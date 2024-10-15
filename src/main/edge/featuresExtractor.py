21e8fcd3ec47c51c61cb700efb888382c6dac492
murasemaru <bp21071@shibaura-it.ac.jp>
2024-10-15 00:11:31 +0000

    edit feature extractor

--- src/main/edge/featuresExtractor.py
+++ src/main/edge/featuresExtractor.py
@@ -16,70 +16,261 @@ import os
 import ipaddress
 import json
 
+
+
 def is_flow_exist(labeled_features, flow_box):
-    for i in flow_box.shape(axis=0): # i番目のフローに特徴量が一致するパケットがあるかどうか
-        if flow_box[i,:,0] == labeled_features[0] and \
-                flow_box[i,:,1] == labeled_features[1] and \
-                    flow_box[i,:,2] == labeled_features[2]:
-            return True
+    flow_id = 0
+    for flow_id in flow_box.shape[0]: # axis 0 = flow_id
+        if flow_box[flow_id,:,0] == labeled_features[0] and \
+                flow_box[flow_id,:,1] == labeled_features[1] and \
+                    flow_box[flow_id,:,2] == labeled_features[2]:
+                        return True,flow_id # if exist return flow_id
         else: pass
-    return False
+    return False,flow_id+1 # not exist
 
 async def remove_flow(delay, flow_box, flow_id,labeled_features):
     #　時間計測を開始
     await asyncio.sleep(delay)
+    # フローに含まれている情報から特徴量を抽出
+
     # 要素を削除（最後の要素を削除）どこの要素を削除するのか？
     flow_box = np.delete(flow_box,flow_id,axis=0)
     return flow_box
 
-def feature_extract_from_flow():
+def labeling_features(pkt):
 
-    return
+    #IPのプロトコル番号が6の時、プロトコルをtcp
+    if pkt[IP].proto == 6:
+        protocol = "tcp"   # feat: protocol(str) : "tcp"
 
-def callback_online(packet):
+    #IPのプロトコル番号が17の時、プロトコルをudp
+    elif pkt[IP].proto == 17:
+        protocol = "udp"   # feat: protocol(str) : "udp"
 
-    # --- Field
-    flow_box = np.array([np.newaxis,np.newaxis,np.newaxis]) # axis [0,flow_id][1,packets][2,features]
-    flow_id = np.array([np.newaxis,np.newaxis])
+    length = int(pkt[IP].len)   # feat: length(int) : 64
+
+    # src : malicious address
+    #パケットの送信元IPアドレスが、指定された悪意のあるIPアドレスのリストに含まれている時
+    if str(pkt[IP].src) in arr_malicious_address_list:
+        addr = str(pkt[IP].dst) # feat: addr(str) : "192.0.2.0"
+        partner = str(pkt[IP].src)  # feat: partner(str) : "192.168.0.1"
+        label = "1"         # feat: label(str)   : "1"
+        direction = "snd"       # feat: direction(str) : "snd"
+        if protocol == "tcp":
+            port = pkt[TCP].sport   # feat: port(int) : 23
+            tcpflag = str(pkt[TCP].flags)  # feat: tcpflag(str) : "S"
+        elif protocol == "udp":
+            port = pkt[UDP].sport
+            tcpflag = "-1"
+
+        #summarize関数に値を送る
+        summarize(day, hour, minute, seconds, protocol, length, addr, partner, label, direction, port, tcpflag)
+
+    # dst : malicious address
+    #パケットの宛先IPアドレスが、指定された悪意のあるIPアドレスのリストに含まれている時
+    elif str(pkt[IP].dst) in arr_malicious_address_list:
+        addr = str(pkt[IP].src)
+        partner = str(pkt[IP].dst)
+        label = "1"
+        direction = "rcv"
+        if protocol == "tcp":
+            port = pkt[TCP].dport
+            tcpflag = str(pkt[TCP].flags)
+        elif protocol == "udp":
+            port = pkt[UDP].dport
+            tcpflag = "-1"
+
+        summarize(day, hour, minute, seconds, protocol, length, addr, partner, label, direction, port, tcpflag)
+
+    # src : benign address
+    #パケットの送信元IPアドレスが、指定された良性のあるIPアドレスのリストに含まれていて、宛先IPアドレスが指定された良性のあるIPアドレスのリストに含まれていない時
+    elif str(pkt[IP].src) in arr_benign_address_list and str(pkt[IP].dst) not in arr_benign_address_list:
+        addr = str(pkt[IP].dst)
+        partner = str(pkt[IP].src)
+        label = "0"
+        direction = "snd"
+        if protocol == "tcp":
+            port = pkt[TCP].sport
+            tcpflag = str(pkt[TCP].flags)
+        elif protocol == "udp":
+            port = pkt[UDP].sport
+            tcpflag = "-1"
+
+        summarize(day, hour, minute, seconds, protocol, length, addr, partner, label, direction, port, tcpflag)
+
+    # dst : benign address
+    #パケットの宛先IPアドレスが、指定された良性のあるIPアドレスのリストに含まれていて、送信元IPアドレスが指定された良性のあるIPアドレスのリストに含まれていない時
+    elif str(pkt[IP].dst) in arr_benign_address_list and str(pkt[IP].src) not in arr_benign_address_list:
+        addr = str(pkt[IP].src)
+        partner = str(pkt[IP].dst)
+        label = "0"
+        direction = "rcv"
+        if protocol == "tcp":
+            port = pkt[TCP].dport
+            tcpflag = str(pkt[TCP].flags)
+        elif protocol == "udp":
+            port = pkt[UDP].dport
+            tcpflag = "-1"
+
+        summarize(day, hour, minute, seconds, protocol, length, addr, partner, label, direction, port, tcpflag)
+
+    return []
+
+#summarize引数
+#day=日付, hour=時間, minute=分, seconds=秒, protocol=プロトコル(tcp/udp), length=IPアドレス長?
+#addr=宛先アドレス, partner=送信元アドレス, label = 1/0, direction=snd/rcv, port=ポート番号, tcpflag= /-1
+def summarize(day, hour, minute, seconds, protocol, length, addr, partner, label, direction, port, tcpflag):
+    global connecting_address_list
+    global connecting_object_list
+    global output_list
+    global timeout_count
+
+    timeout_count += 1
+    #----------------------- first packet(no connection) ---------------------------------
+    #最初のパケット(接続なし)
+
+    #変数addrが、リストconnecting_address_listに含まれていない場合
+    #connecting_address_listにaddrを追加
+    #フローが始まった日時date作成?
+    #FlowFeaturesClassのインスタンスを作成し、変数objに格納
+    #FlowFeaturesClass = フローごとに作成されるオブジェクトのクラスファイル。
+    #このファイルに追記することで抽出する特徴量を増やしたりもできる。alt_val は欠損値の代わりに書き込まれる値。
+    if addr not in connecting_address_list:
+        connecting_address_list.append(addr)
+
+        date = day + hour + minute
+        obj = FlowFeaturesClass(date, addr, partner, label) # create instance
+
+        #drectionがrcv(受信)のとき
+        #オブジェクトの属性rcv_packet_count(受信パケット数)を1増やす
+        #rcv_tmp_timeに秒を格納
+        #rcv_length_list(リスト)にlengthの値を追加
+        if direction == "rcv":
+            obj.rcv_packet_count += 1
+            obj.rcv_tmp_time = seconds
+            obj.rcv_length_list.append(length)
+
+        #drectionがsnd(送信)のとき
+        #snd_packet_count(送信パケット数)を1増やす
+        #snd_tmp_timeに秒を格納
+        #snd_length_list(リスト)にlengthの値を追加
+        elif direction == "snd":
+            obj.snd_packet_count += 1
+            obj.snd_tmp_time = seconds
+            obj.snd_length_list.append(length)
+
+        #protcol_listにprotocolの値追加
+        #port_listにportの値追加
+        obj.protocol_list.append(protocol)
+        obj.port_list.append(port)
+
+        #last_secondsにsecondsの値追加
+        obj.last_seconds = seconds
+
+        #connecting_object_listにオブジェクトobjを追加
+        connecting_object_list.append(obj)
+
+    #----------------------- already connecting ------------------------------------------
+    #すでに接続しているとき
+    #connecting_address_listからaddrがある行の値をaddr_indexに格納
+    else:
+        addr_index = connecting_address_list.index(addr)
+
+        #rcvのとき
+        #connecting_object_listのaddr_indexにあるrcv_packet_count(受信パケット数)を1増やす
+        #connecting_object_list[addr_index]のメソッドcalculateTimeIntervalにsecondsとdirectionの値を入れる
+        #connecting_object_list[addr_index]のrcv_length_listにlengthの値が追加される
+        if direction == "rcv":
+            connecting_object_list[addr_index].rcv_packet_count += 1
+            connecting_object_list[addr_index].calculateTimeInterval(seconds, direction)
+            connecting_object_list[addr_index].rcv_length_list.append(length)
+
+        #sndのとき
+        #connecting_object_listのaddr_indexにあるsnd_packet_count(受信パケット数)を1増やす
+        #connecting_object_list[addr_index]のメソッドcalculateTimeIntervalにsecondsとdirectionの値を送る
+        #connecting_object_list[addr_index]のsnd_length_listにlengthの値が追加される
+        elif direction == "snd":
+            connecting_object_list[addr_index].snd_packet_count += 1
+            connecting_object_list[addr_index].calculateTimeInterval(seconds, direction)
+            connecting_object_list[addr_index].snd_length_list.append(length)
+
+        #connecting_object_list[addr_index]の属性protocol_listにprotocolの値追加
+        #connecting_object_list[addr_index]の属性port_listにportの値追加
+        connecting_object_list[addr_index].protocol_list.append(protocol)
+        connecting_object_list[addr_index].port_list.append(port)
+
+        #connecting_object_list[addr_index]の属性last_secondsにsecondsの値代入
+        connecting_object_list[addr_index].last_seconds = seconds
+
+    #------------------------ timeout check --------------------------------------
+    #タイムアウトになっているか
+    # timeout_check_intervalが条件を満たしたら
+    if timeout_count == timeout_check_interval:
+
+        output_addr_list = []
+
+        #connecting_address_listの全部に対して繰り返し処理
+        #current_time(sniffメソッドを終了するまでの秒数)より小さい場合にoutput_listにconnecting_object_list[l]追加
+        #また、output_addr_listにconnecting_object_list[l]のaddrを追加
+        for l in range(len(connecting_address_list)):
+            if connecting_object_list[l].last_seconds + timeout < current_time:
+                output_list.append(connecting_object_list[l])
+                output_addr_list.append(connecting_object_list[l].addr)
+
+        #output_addr_listの要素に対してconnecting_address_listからout_addrの値を削除
+        for out_addr in output_addr_list:
+            connecting_address_list.remove(out_addr)
+
+            #connecting_object_listのコピーに対しての繰り返し処理(a[:]でリストaのコピーを作成するらしい)
+        #更にoutput_addr_listに対しての繰り返し処理
+        #ob.addrとout_addrが等しいならconnecting_object_listからobの値を削除
+        for ob in connecting_object_list[:]:
+            for out_addr in output_addr_list:
+                if ob.addr == out_addr:
+                    connecting_object_list.remove(ob)
+
+        #timeout_countを0にする
+        timeout_count = 0
+    #-------------------------------------------------------------------------------------
+
+def feature_extract(pkt):
+    # パケットから各種特徴量を抽出
+    explanatory_variable = []
+    return explanatory_variable
+
+def callback(pkt):
+
+    print(pkt.show())
+    flow_box = np.array([np.newaxis,np.newaxis,np.newaxis]) # axis [0,flow_id][1,packet_id][2,features]
+    feature_matrix = pd.DataFrame() # mainから引っ張ってくるように修正
 
     # --- IPv4 or not
-    if packet[Ether].type == 0x0800 and \
-            packet[IP].src not in ex_addr_list and \
-            packet[IP].dst not in ex_addr_list and \
-            (packet[IP].proto == 6 or packet[IP].proto == 17):
-        print(packet.summuerize())
+    if pkt[Ether].type == 0x0800 and \
+        pkt[IP].src not in ex_addr_list and \
+        pkt[IP].dst not in ex_addr_list and \
+        (pkt[IP].proto == 6 or pkt[IP].proto == 17):
 
-        # 特徴情報を抽出
-        explanatory_variable_array = ["","",""]
-
-        # パケットのラベル付けを行う(これはフロー確定後でも良いかも)
-        target_variable = [0]
-
-        # ラベル付けされた特徴量
-        labeled_features = ["","","",""]
-
-        # フロー番号，パケット番号，特徴量リストをもつ3次元配列を定義
+        # フローのラベリング、特徴量の抽出
+        explanatory_variable = feature_extract(pkt)
 
         # もしパケットが新しいフローであると判断できる場合
-        if not is_flow_exist(labeled_features,flow_box):
-            flow_box = np.concatenate([flow_box, labeled_features], axis=0)
-            flow_id = [-1,flow_box.shape[0]]
-            remove_flow(3600,flow_box,flow_id,labeled_features)
-
+        # 新しいflow_idにlabeled_featuresを追加
+        # 追加してから60秒待機 その間他のパケットによる同一フローへの追加を許可する
+        # つまりこのコールバック関数を待機状態にしたうえで他のコールバック関数が呼ばれるように処理を一時的に他のプロセスに受け渡す必要がある
+        # 待機が終了したらフローから特徴量を抽出する
+        flow_exist,flow_id = is_flow_exist(explanatory_variable,flow_box)
+        if not flow_exist:
+            # 作成したフローは別スレッドで動かす必要がある
+            flow_box = np.concatenate([flow_box, explanatory_variable], axis=0)
+            remove_flow(3600,flow_box,flow_id,explanatory_variable)
+            # フローをラベリング
+            labeled_features = labeling_features(pkt)
+            # 指定時間後，フローが終了したと判断された場合，特徴量行列に書き込む
+            labeled_features_matrix = [-1,labeled_features]
         # もしパケットが以前のフローであると判断できる場合
         else:
-        # フローリストの該当フローリストに特徴量配列を追加
-            flow_box[1].add(explanatory_variable_array)
-        # 指定時間後，フローが終了したと判断された場合
-        # まとまった状態で特徴量を抽出する
-
-    # flow_boxを次の処理に渡す必要がある
-    return
-
-def callback_offline(packet):
-    return
-
-
+            # フローリストの該当フローリストに特徴量配列を追加
+            flow_box[flow_id] = explanatory_variable
 
 def write_csv(feature_matrix_list, feature_matrix, output_dir):
     explanatory_variable = pd.concat([feature_matrix, pd.DataFrame(feature_matrix_list, columns=feature_matrix.columns)])
@@ -88,15 +279,18 @@ def write_csv(feature_matrix_list, feature_matrix, output_dir):
 def online():
     print("online mode")
     while 1:
-        sniff(prn = callback_online, timeout = captime, store = False)
+        sniff(prn = callback, timeout = captime, store = False)
 
 def offline(file_path):
-            if os.path.getsize(file_path) == 0:
-                print(file_path + ":no data")
-            else:
-                print(file_path + ":sniffing...")
-                sniff(offline = file_path, prn = callback_offline, store = False)
-                print(file_path + ":finish")
+    print("offline mode")
+    if os.path.getsize(file_path) == 0:
+        print(file_path + ":no data")
+    else:
+        print(file_path + ":sniffing...")
+        sniff(offline = file_path, prn = callback, store = False)
+        print(file_path + ":finish")
+
+    return feature_matrix_list
 
 # パターン1
 # settings.jsonを読み込む done
@@ -124,7 +318,7 @@ def offline(file_path):
 # すべてのpcapファイルの読み込みが終わるまで継続
 # すべてのpcapファイルについて特徴量の抽出が完了したら，feature_matrix_listをまとめてfeature_matrixに登録
 # feature_matrixをもとにcsvファイルを出力
-# とてつもない大きさの配列リストを保持し続ける必要があるという懸念
+# とてつもない大きさの配列リストを保持し続ける必要があるという懸念 DataFrameで保持する必要はあるのか
 # しかし全ての特徴量が同一csvファイルにあるのは便利，実環境に近い
 
 
@@ -139,6 +333,7 @@ if __name__ == "__main__":
     init_time: str = datetime.now(jst).strftime("%Y%m%d%H%M%S")
 
     # --- Field
+
     online_mode = settings["FeatureExtract"]["ONLINE_MODE"]
     traffic_data_path = settings["FeatureExtract"]["TRAFFIC_DATA_PATH"]
     feature_matrix_column = settings["FeatureExtract"]["EXPLANATORY_VARIABLE_COLUMN"]
@@ -200,13 +395,15 @@ if __name__ == "__main__":
             print("folder")
             for pcap_file in os.listdir(traffic_data_path):
                 pcap_file_path:str=os.path.join(traffic_data_path,pcap_file)
-                offline(pcap_file_path)
-                write_csv(feature_matrix_list=feature_matrix_list,feature_matrix=feature_matrix,output_dir=outputs_dir_path)
+                feature_matrix_list = offline(pcap_file_path)
+                feature_matrix = pd.DataFrame(feature_matrix_list, columns=feature_matrix.columns)
+                feature_matrix.to_csv(f"{outputs_dir_path}/results_training.csv",index=False)
             print("all pcap file sniffed")
         elif os.path.isfile(traffic_data_path):
             print("file")
-            offline(traffic_data_path)
-            write_csv(feature_matrix_list=feature_matrix_list,feature_matrix=feature_matrix,output_dir=outputs_dir_path)
+            feature_matrix_list = offline(traffic_data_path)
+            feature_matrix = pd.DataFrame(feature_matrix_list, columns=feature_matrix.columns)
+            feature_matrix.to_csv(f"{outputs_dir_path}/results_training.csv",index=False)
             print("all pcap file sniffed")
         else:
             print(f"traffic data path : {traffic_data_path} not found")
---
+++ src/test/a.py
@@ -0,0 +1,7 @@
+name_list = ['たろう', 'じろう', 'さぶろう', 'しろう', 'ごろう']
+
+i = 0
+for i in name_list:
+    print(10)
+
+print(i)
