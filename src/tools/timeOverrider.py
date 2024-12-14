40be47cbe698fdb09e788a7d43e11cf259f241d1
Shuji Murase <105190016+murasemaru@users.noreply.github.com>
2024-12-14 22:35:40 +0900

    Update timeOverrider.py

--- src/tools/timeOverrider.py
+++ src/tools/timeOverrider.py
@@ -4,25 +4,25 @@ from datetime import timedelta, datetime
 import pandas as pd
 
 # --- csvファイルのtimestampを基準値に沿って平行移動するスクリプト --------------------------------------------------------------------- #
-beginning_timestamp = "2022-01-01 09:02:39"
-target_dir_path = "src/main/traffic_data/csv/unprocessed/AusEast202201"
+beginning_timestamp = "2022-01-01 09:03:08"
+target_dir_path = "src/main/traffic_data/csv/unprocessed/Lab01"
 # --- Create output directory
 dt = datetime.strptime(beginning_timestamp, "%Y-%m-%d %H:%M:%S")
 formatted_date = dt.strftime("%Y%m%d")
-output_dir_path: str = f"src/main/traffic_data/csv/{os.path.basename(target_dir_path)}-fetch{formatted_date}"
+output_dir_path: str = f"src/main/traffic_data/csv/modified/{os.path.basename(target_dir_path)}-fetch{formatted_date}"
 os.makedirs(output_dir_path)
 # ---------------------------------------------------------------------------------------------------------------------------------- #
 
 first_reading_flag = True
 shift = None
 print("time override")
+
 for dataset_file in sorted(os.listdir(target_dir_path)):
     print(f"{dataset_file} : processing")
     dataset_file_path = os.path.join(target_dir_path,dataset_file)
     df = pd.read_csv(dataset_file_path)
     df["daytime"] = pd.to_datetime(df["daytime"], format="%Y-%m-%d %H:%M:%S")
 
-    
     if df.empty:  # データフレームが空の場合はスキップ
         print(f"{dataset_file} is empty. Skipping...")
         continue
