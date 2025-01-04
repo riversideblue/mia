ebb47bf22604a0dd8de578074fc9736ee176bda2
murasemaru <bp21071@shibaura-it.ac.jp>
2025-01-04 07:18:44 +0000

    dp little fix

--- src/util/1/driftPlotter.py
+++ src/util/1/driftPlotter.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 
 drift_dir_path = "/mnt/nas0/g005/murasemaru/exp/1_DataAnalytics/drift"
 file_name = "mean_dist"
-metrix = ["nmr_w_mean_dist","nmr_ks_mean_dist","nmr_mean_dist"]
+metrix = ["w_mean_dis","ks_mean_dis","mean_dis"]
 label_size = 22
 ticks_size = 16
 legend_size = 22
 
 for di in os.listdir(drift_dir_path):
     di_path = f"{drift_dir_path}/{di}"
-    output_dir_path = os.path.join(os.path.dirname(di_path), "res_img")
+    output_dir_path = os.path.join(di_path, "res_img")
     os.makedirs(output_dir_path, exist_ok=True)  # ループの外で1回だけ実行
 
     for fi in os.listdir(di_path):
@@ -24,12 +24,12 @@ for di in os.listdir(drift_dir_path):
             ex_val = fi.replace("with_population_", "").replace(".csv", "")
             output_path = f"{output_dir_path}/{file_name}_{ex_val}.png"
             
-            data['daytime'] = pd.to_datetime(data['daytime'])
+            data['date'] = pd.to_datetime(data['date'])
             plt.figure(figsize=(12, 8))
             for y in metrix:
-                plt.plot(data['daytime'], data[y], label=y, linewidth=2)
+                plt.plot(data['date'], data[y], label=y, linewidth=2)
             
-            plt.xlabel('daytime', fontsize=label_size)
+            plt.xlabel('date', fontsize=label_size)
             
             plt.xticks(fontsize=ticks_size, rotation=45)
             plt.yticks(fontsize=ticks_size)
