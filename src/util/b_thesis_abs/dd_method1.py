fe49ecc0fe2347f7a7b76f54a2a4c341c600891f
murasemaru <bp21071@shibaura-it.ac.jp>
2025-01-16 08:01:53 +0000

    dd_method

--- src/util/b_thesis_abs/dd_method1.py
+++ src/util/b_thesis_abs/dd_method1.py
@@ -6,9 +6,9 @@ import japanize_matplotlib
 
 # プロットするデータ
 current_points = [
-    (0.28249593, 1.44140857, 0.50995917), #1
-    (0.13087773, 1.02789779, -0.09376296), #2
-    (0.28964872, 1.00561939, 1.53686038) #3
+    (0.25249593, 1.54140857, 0.00995917), #1
+    (0.65087773, 1.22789779, -0.10376296), #2
+    (0.50964872, -0.30561939, 0.83686038) #3
 ]
 
 past_points = [
@@ -16,12 +16,12 @@ past_points = [
     (-0.59968694, 2.11819066, 0.05367675), #2
     (0.94988517, 1.00705003, 1.08114783), #3
     (0.61716652, 0.39894359, -0.02070926), #4
-    (0.30207219, 0.33025866, -0.04522768) #5
+    (0.50207219, -0.43025866, -0.04522768) #5
 ]
 
 # 保存先
 output_path = '/mnt/nas0/g005/murasemaru/exp/other'
-file_name = 'dd_mtrhod1.png'
+file_name = 'dd_method1.png'
 if not os.path.exists(output_path):
     os.makedirs(output_path)
 
@@ -31,11 +31,11 @@ ax = fig.add_subplot(111, projection='3d')
 
 # 過去データをプロット
 for i, (x, y, z) in enumerate(past_points):
-    ax.scatter(x, y, z, color='red', alpha=0.7, s=130)
+    ax.scatter(x, y, z, color='red', alpha=0.7, s=300)
 
 # 現在データをプロット
 for i, (x, y, z) in enumerate(current_points):
-    ax.scatter(x, y, z, color='green', alpha=0.7, s=130)
+    ax.scatter(x, y, z, color='green', alpha=0.7, s=300)
 
 # 軸範囲を設定（データ範囲に基づいて適切な範囲を選択）
 ax.set_xlim(-1, 2)
